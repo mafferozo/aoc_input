94422d5ee6404305ab7fe037b7bf422f90730a27
Martijn van Erne <martijn.van.erne@nl.abnamro.com>
2023-02-06 23:39:25 +0100

    Remove result as return type from CLI binary

--- src/bin/aoc-input/main.rs
+++ src/bin/aoc-input/main.rs
@@ -8,13 +8,9 @@ use aoc_input_lib::get_puzzle_input;
 
 use args::Args;
 
-type AppResult<T> = Result<T, Box<dyn Error + Send + Sync>>;
-
-fn main() -> AppResult<()> {
+fn main()  {
     let cli = Args::parse();
 
     let input = get_puzzle_input(cli.year,cli.day,cli.session)?;
     print!("{}", input);
-
-    Ok(())
 }
